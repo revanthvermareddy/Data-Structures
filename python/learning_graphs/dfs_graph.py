0f7642fe566535d5c0a01a532b51bc933782438f
Revanth Vermareddy <vermareddyrevanth@gmail.com>
2023-07-01 19:48:23 +0530

    code changes to DFS

--- python/learning_graphs/dfs_graph.py
+++ python/learning_graphs/dfs_graph.py
@@ -1,44 +1,25 @@
 # Adjacency List (we use HashMap Data Structure)
 
-
-graph = {
-    'a': ['b', 'c'],
-    'b': ['d'],
-    'c': ['e'],
-    'd': [],
-    'e': ['b'],
-    'f': ['d']
-}
-
 def depthFirstStack(graph, source):
-    
     stack = [source]
-    
     visted = set()
-    
     while len(stack) > 0:
         current = stack.pop()
-        
         if current in visted: continue
-        
         print(current)
-        
         # add the neighbours into stack
         for neighbour in graph[current]:
             stack.append(neighbour)
-        
         visted.add(current)
 
-# depthFirstStack(graph, 'a') 
-
-
-
-def depthFirstStack(graph, source):
-    print(source)
-    for neighbour in graph[source]:
-        depthFirstStack(graph, neighbour)
-
-        
-depthFirstStack(graph, 'a')   
-        
-        
+# driver code
+if __name__ == "__main__":
+    graph = {
+        'a': ['b', 'c'],
+        'b': ['d'],
+        'c': ['e'],
+        'd': [],
+        'e': ['b'],
+        'f': ['d']
+    }
+    depthFirstStack(graph, 'a') 
